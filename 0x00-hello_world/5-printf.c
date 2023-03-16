b63a7f49479046e8ab9e296cd17d0f773e02b6d8
MKihungi <mwangim4786@example.com>
2023-03-16 13:44:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - is the main function
+ *
+ * Return: always return zero (0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
