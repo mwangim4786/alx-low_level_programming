8bd9f0fd43ad7c4d3d7889e964efe15b1f601005
MKihungi <mwangim4786@example.com>
2023-03-17 18:21:47 -0700

    Clear errors

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 	printf("Last digit of %d is %d", n, x);
 	if (x > 5)
 	{
-		printf("and is greater than 5");
+		printf(" and is greater than 5");
 	}
 	else if (x == 0)
 	{
-		printf("and is 0");
+		printf(" and is 0");
 	}
 	else if (x < 6 && x != 0)
 	{
-		printf("and is less than 6 and not 0");
+		printf(" and is less than 6 and not 0");
 	}
 	return (0);
 }
