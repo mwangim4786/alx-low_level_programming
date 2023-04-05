c570752249b6b60ebdd9559cd2e8ad7200906c6e
MKihungi <mwangim4786@example.com>
2023-04-05 06:34:48 -0700

    Removed errors

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,15 +6,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int i, n;
-
-	n = 0;
-	while (s[n] != '\0')
-		n++;
-
-	for (i = n - 1; i >= 0; i--)
+	if (*s)
 	{
-		_putchar(s[i]);
 		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
