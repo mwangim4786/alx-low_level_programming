faf27f4890cfa5d9e76fc814f05e58f967536961
MKihungi <mwangim4786@example.com>
2023-04-13 15:31:11 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - terminates process if malloc fails
+ *
+ * @b: allocated memory
+ *
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *i;
+
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	return (i);
+
+}
