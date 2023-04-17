cf3aaf7d2a0a4ae18586d38d1f4831349f30e40b
MKihungi <mwangim4786@example.com>
2023-04-17 06:13:30 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Readme for project 0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog - Structure for dog
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
