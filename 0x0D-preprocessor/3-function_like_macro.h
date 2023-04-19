f7877cfccc2bc8371a3061f7f6a010e193e2349d
MKihungi <mwangim4786@example.com>
2023-04-18 23:20:17 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#ifndef ABS(x)
+
+#define ABS(x)
+
+#endif
+
+/**
+ * main - the mai function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        printf("%s\n", __FILE__);
+
+        return (0);
+}
