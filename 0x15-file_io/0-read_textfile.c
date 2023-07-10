b4773915dc4a73ba8258f24be9d7356eb04b0501
Martin Kihungi <mwangi4786@gmail.com>
2023-07-10 05:45:10 -0700

    0-read_textfile.c try 6

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -31,15 +31,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	_read = read(_open, no_of_letters, letters);
 	_write = write(2, no_of_letters, _read);
 
-	if (_open == -1 || _read == -1 || _write == -1 || _write != _read)
+	if (_open == -1 || _read == -1 || _write == -1 || _write != no_of_letters)
 	{
 		free(no_of_letters);
 		return (0);
 	}
-
-	if(_write != letters)
-		free(no_of_letters);
-		return (0);
 
 	free(no_of_letters);
 	close(_open);
