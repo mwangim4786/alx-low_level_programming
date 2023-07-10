37b33827f4cfd9aacc0bcf16dfd0b59433da099c
Martin Kihungi <mwangi4786@gmail.com>
2023-07-10 05:36:06 -0700

    0-read_textfile.c try 5

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -37,7 +37,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	if(_write != no_of_letters)
+	if(_write != letters)
+		free(no_of_letters);
 		return (0);
 
 	free(no_of_letters);
